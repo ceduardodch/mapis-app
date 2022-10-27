13f7656188f78e5bcb0bba9f66559b2151b5525c
ceduardodch <ceduardodch@gmail.com>
2022-10-27 11:58:52 -0500

    CD

--- android/app/src/test/java/com/getcapacitor/myapp/ExampleUnitTest.java
+++ android/app/src/test/java/com/getcapacitor/myapp/ExampleUnitTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see <a routerLink="/http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
 
